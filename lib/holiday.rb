db9a5543440e00df7120a1ca8a2249a6a5a5359d
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-12 16:48:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
 
 end
 def all_holidays_with_bbq(holiday_hash)
+  holiday_hash.each do |holiday,supplies|
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
